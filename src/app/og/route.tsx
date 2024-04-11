d6318d16004388e1a26c918f1da2e968557e3a89
Andrew Gillingham <andrewdgillingham@gmail.com>
2024-04-11 14:48:14 -0400

    chore: make og smaller

--- src/app/og/route.tsx
+++ src/app/og/route.tsx
@@ -4,12 +4,10 @@ import { ImageResponse } from 'next/og';
 
 export const runtime = 'edge';
 
-const regularFont = fetch(new URL('/public/assets/fonts/inter/inter-regular.ttf', import.meta.url)).then((res) => res.arrayBuffer());
-
 const boldFont = fetch(new URL('/public/assets/fonts/inter/inter-bold.ttf', import.meta.url)).then((res) => res.arrayBuffer());
 
 export async function GET() {
-  const [regularFontData, boldFontData] = await Promise.all([regularFont, boldFont]);
+  const boldFontData = await boldFont;
   return new ImageResponse(
     (
       <div
@@ -91,7 +89,7 @@ export async function GET() {
               style={{
                 fill: '#0d9488',
               }}
-              d="M96 64c0-17.7 14.3-32 32-32h32c17.7 0 32 14.3 32 32V224v64V448c0 17.7-14.3 32-32 32H128c-17.7 0-32-14.3-32-32V384H64c-17.7 0-32-14.3-32-32V288c-17.7 0-32-14.3-32-32s14.3-32 32-32V160c0-17.7 14.3-32 32-32H96V64zm448 0v64h32c17.7 0 32 14.3 32 32v64c17.7 0 32 14.3 32 32s-14.3 32-32 32v64c0 17.7-14.3 32-32 32H544v64c0 17.7-14.3 32-32 32H480c-17.7 0-32-14.3-32-32V288 224 64c0-17.7 14.3-32 32-32h32c17.7 0 32 14.3 32 32zM416 224v64H224V224H416z"
+              d="M96 64c0-17.7 14.3-32 32-32h32c17.7 0 32 14.3 32 32v384c0 17.7-14.3 32-32 32h-32c-17.7 0-32-14.3-32-32v-64H64c-17.7 0-32-14.3-32-32v-64c-17.7 0-32-14.3-32-32s14.3-32 32-32v-64c0-17.7 14.3-32 32-32h32V64zm448 0v64h32c17.7 0 32 14.3 32 32v64c17.7 0 32 14.3 32 32s-14.3 32-32 32v64c0 17.7-14.3 32-32 32h-32v64c0 17.7-14.3 32-32 32h-32c-17.7 0-32-14.3-32-32V64c0-17.7 14.3-32 32-32h32c17.7 0 32 14.3 32 32zM416 224v64H224v-64h192z"
             />
           </svg>
         </span>
@@ -101,11 +99,6 @@ export async function GET() {
       width: 1200,
       height: 600,
       fonts: [
-        {
-          name: 'Inter',
-          data: regularFontData,
-          weight: 400,
-        },
         {
           name: 'Inter',
           data: boldFontData,
